6623d98cf960f35516864faf37509bcc2d074bfe
Nitin <javabynitin2022@gmail.com>
2024-06-04 09:27:09 +0530

    made changes in Application.java

--- src/main/java/in/pwskills/nitin/Application.java
+++ src/main/java/in/pwskills/nitin/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		int data =10;
 		SpringApplication.run(Application.class, args);
 	}
 
