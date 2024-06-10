1ce6b94da16d01486f4faa109ced7c63943e6169
Nitin Manjunath <javabynitin2024@gmail.com>
2024-06-10 16:50:21 +0530

    Add files via upload
    New Project added

---
+++ 01-Springboot-core-app/src/main/java/in/pwskills/nitin/Application.java
@@ -0,0 +1,16 @@
+package in.pwskills.nitin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+
+		int data =10;
+		String name="change in project source code";
+		SpringApplication.run(Application.class, args);
+	}
+
+}
